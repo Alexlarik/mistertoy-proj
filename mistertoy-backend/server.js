41100ae7db906c708d9a7ed99852d48cc4427967
alexl <alexlarik13@gmail.com>
2024-07-20 16:16:28 +0300

    Refactor the backend server.js and toy.service.js, filling in the skeleton plus connecting the dots

--- mistertoy-backend/server.js
+++ mistertoy-backend/server.js
@@ -7,6 +7,19 @@ import { loggerService } from './services/logger.service.js'
 import { utilService } from './services/util.service.js'
 
 const app = express()
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:8080',
+        'http://localhost:8080',
+
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
+
+        'http://127.0.0.1:5174',
+        'http://localhost:5174',
+    ],
+    credentials: true
+}
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
@@ -15,53 +28,71 @@ app.use(cors(corsOptions))
 // Express Routing:
 
 app.get('/api/toy', (req, res) => {
-
+    const { filterBy = {} } = req.query
     toyService.query(filterBy)
-        .then(toys => res.send(toys))
+        .then(toys => {
+            res.send(toys)
+        })
         .catch(err => {
-            loggerService.error(`Couldn't get toys...`, err)
-            res.status(500).send(`Couldn't get toys...`)
+            loggerService.error('Cannot load toys', err)
+            res.status(400).send('Cannot load toys')
         })
 })
 
 app.put('/api/toy/:id', (req, res) => {
-
-    toyService.save(toyToSave)
-        .then(savedToy => res.send(savedToy))
+    const { txt, price, _id } = req.body
+    const toy = {
+        txt,
+        price: +price,
+        _id,
+    }
+    toyService.save(toy)
+        .then(savedToy => {
+            res.send(savedToy)
+        })
         .catch(err => {
-            loggerService.error(`Couldn't save toys (${_id})`, err)
-            res.status(500).send(`Couldn't save toys (${_id})`)
+            loggerService.error('Cannot update toy', err)
+            res.status(400).send('Cannot update toy')
         })
 })
 
 app.post('/api/toy/', (req, res) => {
-
-    toyService.save(toyToSave)
-        .then(savedToy => res.send(savedToy))
+    const { txt, price } = req.body
+    const toy = {
+        txt,
+        price: +price,
+    }
+    toyService.save(toy)
+        .then(savedToy => {
+            res.send(savedToy)
+        })
         .catch(err => {
-            loggerService.error(`Couldn't save toys...`, err)
-            res.status(500).send(`Couldn't save toys...`)
+            loggerService.error('Cannot add toy', err)
+            res.status(400).send('Cannot add toy')
         })
 })
 
 app.get('/api/toy/:id', (req, res) => {
-    const { id } = req.params
-
-    toyService.getById(id).then((toy) => res.send(toy))
+    const { toyId } = req.params
+    toyService.get(toyId)
+        .then(toy => {
+            res.send(toy)
+        })
         .catch(err => {
-            loggerService.error(`Couldn't get toy (${id})`, err)
-            res.status(500).send(`Couldn't get toy (${id})`)
+            loggerService.error('Cannot get toy', err)
+            res.status(400).send(err)
         })
 })
 
 app.delete('/api/toy/:id', (req, res) => {
-    const { id } = req.params
-
-    toyService.remove(id)
-        .then(() => res.send(`toy ${id} deleted...`))
+    const { toyId } = req.params
+    toyService.remove(toyId)
+        .then(msg => {
+            res.send({ msg, toyId })
+        })
         .catch(err => {
-            loggerService.error(`Couldn't delete toy (${id})`, err)
-            res.status(500).send(`Couldn't delete toy (${id})`)
+            loggerService.error('Cannot delete toy', err)
+            res.status(400).send('Cannot delete toy, ' + err)
         })
 })
 
--- mistertoy-backend/services/toy.service.js
+++ mistertoy-backend/services/toy.service.js
@@ -1,33 +1,119 @@
-import axios from "axios"
-const BASE_URL = '/api/toy'
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+// _createToys()
 
 export const toyService = {
     query,
-    getById,
-    save,
+    get,
     remove,
-    createDefaultFilter,
+    save,
+    getToy,
+    getDefaultFilter,
+
 }
+const toys = utilService.readJsonFile('data/toy.json')
+// For Debug (easy access from console):
+window.cs = toyService
 
 function query(filterBy = {}) {
-    return axios.get(BASE_URL, { params: filterBy }).then(res => res.data)
+    let filteredToys = toys
+    if (filterBy.txt) {
+        const regExp = new RegExp(filterBy.txt, 'i')
+        filteredToys = filteredToys.filter(toy => regExp.test(toy.txt))
+    }
+    if (filterBy.inStock !== '') {
+        filteredToys = filteredToys.filter(toy => {
+            if (filterBy.inStock === 'true') return toy.inStock === true
+            if (filterBy.inStock === 'false') return toy.inStock === false
+            return true
+        })
+    }
+    return Promise.resolve(filteredToys)
 }
-function getById(toyId) {
-    return axios.get(BASE_URL + `/${toyId}`).then(res => res.data)
+
+function get(toyId) {
+    const toy = toys.find(toy => toy._id === toyId)
+    if (!toy) return Promise.reject('Toy not found')
+    return Promise.resolve(toy)
 }
+
 function remove(toyId) {
-    return axios.delete(BASE_URL + `/${toyId}`).then(res => res.data)
+    const idx = toys.findIndex(toy => toy._id === toyId)
+    if (idx === -1) return Promise.reject('No such toy')
+    toys.splice(idx, 1)
+    return _saveToysToFile()
 }
+
 function save(toy) {
-    // const {_id , description, title, createdAt , severity} = toy
-    if (toy.id) {
-        return axios.put(BASE_URL + `/${toy.id}` + toy)
-            .then(res => res.data)
+    if (toy._id) {
+        // TOY - updatable fields
+        toy.updatedAt = Date.now()
+        // return storageService.put(TOY_KEY, toy)
     } else {
-        return axios.post(BASE_URL, toy)
-            .then(res => res.data)
+        toy.createdAt = toy.updatedAt = Date.now()
+        toy.txt = utilService.makeLorem(1)
+        toy.price = utilService.getRandomIntInclusive(5, 80)
+        toy.inStock = toy.inStock ? true : false
+
+        // return storageService.post(TOY_KEY, toy)
+    }
+    return _saveToysToFile().then(() => toy)
+}
+
+function getToy() {
+    return { txt: '', price: '', inStock: '' }
+}
+
+function getDefaultFilter() {
+    return { txt: '', price: '', inStock: '' }
+}
+
+function _createToys() {
+    let toys = utilService.loadFromStorage(TOY_KEY)
+    if (!toys || !toys.length) {
+        toys = []
+        const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle', 'Outdoor', 'Battery Powered']
+        for (var i = 0; i < 20; i++) {
+            // toys.push(_createToy())
+            toys.push(_createToy(null, null, Math.random() > 0.5))
+        }
+        // for (let i = 0; i < 20; i++) {
+        //     const toy = toys[utilService.getRandomIntInclusive(0, txts.length - 1)]
+        //     toys.push(_createToy(toy + (i + 1), utilService.getRandomIntInclusive(1, 10)))
+        // }
+        utilService.saveToStorage(TOY_KEY, toys)
     }
 }
-function createDefaultFilter() {
-    return { txt: '' }
+
+function _createToy(txt, price, inStock) {
+    const toy = getToy(txt, price, inStock)
+    toy.txt = utilService.makeLorem(1)
+    toy.price = utilService.getRandomIntInclusive(5, 80)
+    toy.inStock = inStock ? true : false
+    toy._id = utilService.makeId()
+    toy.createdAt = toy.updatedAt = Date.now() - utilService.getRandomIntInclusive(0, 1000 * 60 * 60 * 24)
+    return toy
 }
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject) => {
+        const toysStr = JSON.stringify(toys, null, 4)
+        fs.writeFile('data/toy.json', toysStr, err => {
+            if (err) {
+                return console.log(err)
+            }
+            resolve()
+        })
+    })
+}
+// const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+//     'Outdoor', 'Battery Powered']
+// const toy = {
+//     _id: 't101',
+//     name: 'Talking Doll',
+//     price: 123,
+//     labels: ['Doll', 'Battery Powered', 'Baby'],
+//     createdAt: 1631031801011,
+//     inStock: true,
+// }
