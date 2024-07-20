ba2c1693adfac736d1d79be22107fcea263efba9
alexl <alexlarik13@gmail.com>
2024-07-20 14:38:49 +0300

    Add toy.service using axios for refactor later

--- mistertoy-backend/services/toy.service.js
+++ mistertoy-backend/services/toy.service.js
@@ -1,5 +1,33 @@
-
+import axios from "axios"
+const BASE_URL = '/api/toy'
 
 export const toyService = {
+    query,
+    getById,
+    save,
+    remove,
+    createDefaultFilter,
+}
 
+function query(filterBy = {}) {
+    return axios.get(BASE_URL, { params: filterBy }).then(res => res.data)
+}
+function getById(toyId) {
+    return axios.get(BASE_URL + `/${toyId}`).then(res => res.data)
+}
+function remove(toyId) {
+    return axios.delete(BASE_URL + `/${toyId}`).then(res => res.data)
+}
+function save(toy) {
+    // const {_id , description, title, createdAt , severity} = toy
+    if (toy.id) {
+        return axios.put(BASE_URL + `/${toy.id}` + toy)
+            .then(res => res.data)
+    } else {
+        return axios.post(BASE_URL, toy)
+            .then(res => res.data)
+    }
+}
+function createDefaultFilter() {
+    return { txt: '' }
 }
